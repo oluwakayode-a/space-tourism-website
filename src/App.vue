c24eaca79115584f41514b68c93223a31c037739
Kayode Oladipo <khaox07@gmail.com>
2022-05-14 06:02:31 +0100

    Completed large nav

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
 <div class="overflow-x-hidden overflow-y-auto relative h-screen inner-container" :class="selected">
   <div class="py-5 px-3 sm:py-0 sm:px-0">
-    <div class="flex justify-between items-center">
-      <span class="logo h-6 w-6 sm:h-10 sm:w-10 sm:my-5 mx-3">
+    <div class="flex justify-between items-center lg:my-5">
+      <span class="logo h-6 w-6 lg:h-14 lg:w-14 sm:h-10 sm:w-10 sm:my-5 lg:mx-10 mx-3">
         <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="20" cy="20" r="20" fill="white"/>
           <path fill-rule="evenodd" clip-rule="evenodd" d="M20 0C20 0 20 20 0 20C19.648 20.1428 20 40 20 40C20 40 20 20 39.9999 20C20 20 20 0 20 0Z" fill="#0B0D17"/>
         </svg>
       </span>
+
       <!-- Desktop Navigation -->
-      <div class="bg-gray-800 h-full px-10 hidden sm:block">
-        <nav class="flex">
-          <router-link :to="nav.path" v-for="(nav, index) in navigation" :key="index" class="px-4 py-6" :class="isActive(nav.name) && 'border-b-2 border-gray-200'">
-            <span class="s-text-con uppercase text-">{{ nav.name }}</span>
+      <div id="nav" class="bg-gray-800 lg:bg-gray-700 h-full px-10 hidden sm:block lg:backdrop-filter lg:backdrop-blur-xl lg:bg-opacity-60 lg:w-3/5">
+        <nav class="flex justify-center items-center">
+          <router-link :to="nav.path" v-for="(nav, index) in navigation" :key="index" class="px-4 py-6 lg:py-8 tracking-widest lg:hover:border-b-2 lg:hover:border-gray-500" :class="isActive(nav.name) && 'border-b-2 border-gray-200'">
+            <span class="s-text-con mr-4 font-bold hidden lg:inline">{{ nav.tag }}</span>
+            <span class="s-text-con uppercase tracking-widest">{{ nav.name }}</span>
           </router-link>
         </nav>
       </div>
       <MenuIcon class="h-6 w-6 cursor-pointer sm:hidden" aria-hidden="true" @click="toggleSide" />
     </div>
+
     <!-- Mobile Navigation -->
     <div class="sm:hidden h-screen z-10 w-9/12 absolute right-0 inset-y-0 transition duration-200 ease-in-out backdrop-filter backdrop-blur-xl bg-opacity-60 translate-x-full" id="sidebar">
       <nav class="px-8 py-24 relative" role="navigation">
@@ -91,5 +94,18 @@ watch(router.currentRoute, () => {
 .inner-container {
   background: url('');
   @apply bg-cover;
+}
+
+@media screen and (min-width: 1024px) {
+  #nav:after {
+      background-color: #fff;
+      content: "";
+      height: 1px;
+      opacity: .25;
+      position: absolute;
+      right: calc(100% - 48px);
+      top: 44px;
+      width: calc(100vw - 103px - 100%);
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="text-center pt-10 sm:pt-15 px-5 h-full">
-    <h3 class="s-text-con uppercase tracking-wider text-lg text-gray-300 sm:mt-10">
-      So, you want to travel to
-    </h3>
-    <h1 class="uppercase text-7xl my-5 sm:text-9xl">Space</h1>
+  <div class="text-center pt-10 sm:pt-15 px-5 h-full lg:flex lg:text-left">
+    <div>
+      <h3 class="s-text-con uppercase tracking-wider text-lg text-gray-300 sm:mt-10">
+        So, you want to travel to
+      </h3>
+      <h1 class="uppercase text-7xl my-5 sm:text-9xl">Space</h1>
 
-    <p class="s-text text-gray-200 text-sm leading-relaxed mt-5 sm:px-28">
-      Let’s face it; if you want to go to space, you might as well genuinely go
-      to outer space and not hover kind of on the edge of it. Well sit back, and
-      relax because we’ll give you a truly out of this world experience!
-    </p>
+      <p class="s-text text-gray-200 text-sm leading-relaxed mt-5 sm:px-28 lg:px-0">
+        Let’s face it; if you want to go to space, you might as well genuinely go
+        to outer space and not hover kind of on the edge of it. Well sit back, and
+        relax because we’ll give you a truly out of this world experience!
+      </p>
+    </div>
 
     <router-link to="/destination">
       <button
@@ -55,6 +57,13 @@ button:hover {
 @media (min-width: 640px) {
   .home {
     background: url('../assets/home/background-home-tablet.jpg') no-repeat;
+    @apply bg-cover;
+  }
+}
+
+@media (min-width: 1024px) {
+  .home {
+    background: url('../assets/home/background-home-desktop.jpg') no-repeat;
     @apply bg-cover;
   }
 }
